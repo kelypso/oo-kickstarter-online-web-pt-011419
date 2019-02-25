75d0fdff1814008a376edd51aa0a0fee5557576c
Kelsey Low <kklowlow@gmail.com>
2019-02-25 02:01:38 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -11,6 +11,6 @@ class Project
     # adds backer instance to backer array of project class
     backer.back_project(self) unless backer.backed_projects.include?(self)
     # adds project instance to project array of backer class
-    # if project instance isn't already therel
+    # if project instance isn't already there
   end
 end
